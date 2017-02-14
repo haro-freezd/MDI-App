4d643af2a2085feaa5d6fb7a63dd41abae4f56f8
haro-freezd <harmanmahal@gmail.com>
2017-02-14 09:20:54 -0800

    Initial Commit

---
+++ MDI/ParentForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MDI
+{
+    public partial class MDIApp : Form
+    {
+        public MDIApp()
+        {
+            InitializeComponent();
+        }
+
+        private void openFromFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void saveASToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
